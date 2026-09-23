04e3b40baa18841a122c0f868353cc70a929a0c8
kzktzfmnw2-crypto <kzktzfmnw2@privaterelay.appleid.com>
2026-09-24 01:23:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-const CACHE='cpa-short-final-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('cpa-short-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const CACHE='cpa-short-final-v11';
+const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cpa-short-')&&k!==CACHE).map(k=>caches.delete(k))))
+  .then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
+  if(e.request.method!=='GET') return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return res;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
   e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
-    const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
-  }).catch(()=>caches.match('./index.html'))));
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  })));
 });
